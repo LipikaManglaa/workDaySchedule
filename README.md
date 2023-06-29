f4098191935fca97dc72a85b47b499def7b77072
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-06-29 12:40:13 +1000

    ready work day scheduler

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # workSchedule
+
+This is a Work Day Scheduler application using HTML, CSS, and Jquery. This application uses third party for time "day.js". My Daily Planner is a 8am to 5Pm hour time scheduling application that user can save their hourly work into their schedule planner.it  saves user input to localStorage. This app utilizes jQuery for it's easy setup and day.User can show their data by clicking save and delete by Toast Plugin .JS for presenting the current date and color coding each time block based on the time of day. These colors are:
+
+:green_circle: Indicates the time left in the day 
+
+ :red_circle: Indicates current date 
+
+:white_circle: Indicates that the time has passed
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+https://github.com/LipikaManglaa/quiz.git
+* [GitHub Repository](https://github.com/LipikaManglaa/quiz.git)
+* [Deployed GitHub IO]
+
+
+
+### Prerequisites
+
+To install this application, you will need a text editor. I recommend Visual Studio Code. 
+
+### Installing
+
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository. 
+
+
+
+##  User Story:
+**AS  a busy schedule for everyone**
+- I WANT to add important events to a daily planner
+- SO THAT I can manage my time effectively
+
+**# This project has the following features: **
+- When user opens the planner 
+  - current day and time is displayed at the top 
+
+  ![](screenshot/Screenshot1.png)
+
+
+- When user scrolls down,
+  - They're presented with time blocks using standard business hours
+- When user view the time blocks for that day 
+  - Each time block is color coded indicating past, present, and future
+- When user clicks onto a time block 
+  - They can enter an event
+
+- When the user save data 
+ -it can shows by Toast Plugin  ,data has been saved
+
+ ![](screenshot/Screenshot2.png)
+
+- When the user wants to delete data 
+ -it can shows by Toast Plugin  ,data has been deleted
+
+ ![](screenshot/Screenshot3png)
+ 
+
+ ### This project has media Queries for:
+
+* max-width: 991px 
+    * Adjusts body and container width
+
+* max-width: 567px
+    * Adjusts body and container width
+  
--- script.js
+++ script.js
@@ -12,87 +12,132 @@ $("#currentTime").text(currentWholeTime)
 var currentTime = dayjs().format("HH");
 // console.log(currentTime)
 
-
+//create Array for Time
 let obj = ["08Am", "09Am", "10Am", "11Am", "12Pm", "13Pm", "14Pm", "15Pm", "16Pm", "17Pm"]
+
+
 let data = "";
-let mainSectionArea;
+//declare all variables for create HTML
+let mainSectionArea, mainSectionInner, hourText, hourTextCreate, textAreaValue;
 
 obj.forEach((v, i) => {
   let time = v.slice(0, -2)
   // console.log(time)
-  let childMain = $("<div>").addClass(" row con-main")
+  // create HTML with create element
+  //this is main div 
+  mainSectionInner = $("<div>").addClass("row container-main ")
 
+  //these below three lines create time on th left side of HTML
+  hourText = $("<div>").addClass(" time-slot col-2 col-md-1").attr("id", v);
+  hourTextCreate = $("<div>").text(v).addClass("col-2 col-md-1 hour mobile-size text-center py-3")
+  hourText.append(hourTextCreate);
 
-  let hourCreate = $("<div>").addClass(" time-block col-2 col-md-1").attr("id", v);
-
-  let textCreate = $("<div>").text(v).addClass("col-2 col-md-1 hour text-center py-3")
-
-  hourCreate.append(textCreate);
-
-  let taskArea = $("<textarea>").addClass("col-6 col-md-9 description rows=3")
+  //textareaa which we create textvalue 
+  textAreaValue = $("<textarea>").addClass("col-6 col-md-9 description rows=3")
 
   //for save btn
   let btnSave = $("<button>").addClass("btn saveBtn col-2 col-md-1 ")
-
   let saveIcon = $("<i>").addClass("fas fa-save")
-
   btnSave.append(saveIcon);
 
   //for delete btn
   let btnDelete = $("<button>").addClass("btn deleteBtn col-2 col-md-1")
-
   let btnIcon = $("<i>").addClass("fas fa-trash")
-
   btnDelete.append(btnIcon)
 
 
   // append everything to this row
-  childMain.append(hourCreate, taskArea, btnSave, btnDelete);
+  mainSectionInner.append(hourText, textAreaValue, btnSave, btnDelete);
 
-
-   mainSectionArea = $(".working-schedule").append(childMain);
+  //in the last HTML append all divs to Main div
+  mainSectionArea = $(".working-schedule").append(mainSectionInner);
 
   if (currentTime == time) {
-    taskArea.addClass("present");
+    textAreaValue.addClass("present");
   } else if (currentTime > time) {
-    taskArea.addClass("past")
+    textAreaValue.addClass("past")
   } else {
-    taskArea.addClass("future")
+    textAreaValue.addClass("future")
   }
 
 })
 
+
+//data store into localstorage by click save button
 var objcreate;
-let textValue,timeCreate
+let textValue, timeId;
 $(".saveBtn").on("click", function () {
-  textValue = ($(this).prev("textarea").val())
-  
-  timeCreate = ($(this).siblings("div").children().text())
  
-   objcreate = {
-    [timeCreate]:textValue
-  }
+  //pick the value from textarea
+  textValue = ($(this).prev("textarea").val())
 
-  var getItemLocal = JSON.parse(localStorage.getItem("workSchedule")) ?? []
+  //pick the id value which we stored as a time
+  timeId = $(this).siblings("div").attr("id");
+  // //after get value from local stroage , then add new value 
+  // var finalData = [...getItemLocal,textValue ]
 
-
-  var finalData = [...getItemLocal, objcreate]
-  // console.Log(finalData)
-  localStorage.setItem("workSchedule", JSON.stringify(finalData))
-  
+  //store value into local storage
+  localStorage.setItem(timeId, JSON.stringify(textValue))
+  displayData()
+  //textarea value empty
   $(this).prev("textarea").val("")
+  $.toast({
+    text:"Data has been Saved!!",
+    icon: 'success',
+      position : 'top-right' ,
+    hideAfter : 5000, 
+    showHideTransition : 'slide'
+  })
 })
 
-let taskData;
+//display data into textarea
+let timeIdLocal, getTextId, getItemLocal;
 
-function displayData(){
+function displayData() {
   $("textarea").each(function () {
-    getItemLocal = JSON.parse(localStorage.getItem("workSchedule")) ?? [] 
-    console.log(getItemLocal)
-    timeCreate = ($(this).siblings("div").children().text())
-    // console.log(timeCreate)
+    let textArea = $(this);
+
+    //get localstorage keys
+    for (let i = 0; i < localStorage.length; i++) {
+
+      // get the value set over each key
+      timeIdLocal = localStorage.key(i);
+      // console.log(timeIdLocal)
+
+      //get value for Id for matching whic saved on localstorage
+      getTextId = $(this).siblings("div").attr("id");
+
+      if (timeIdLocal == getTextId) {
+
+        //get value if there is any value in localstorage 
+        let getItemLocal = JSON.parse(localStorage.getItem(getTextId))
+
+        textArea.val(getItemLocal);
+        
+      }
+    }
+
   })
-  
 }
 displayData()
 
+//delete data
+$(".deleteBtn").click(function() {
+ 
+      // get the id 
+      let textId = $(this).siblings("div").attr("id");
+
+      // Set the  textarea value to be empty
+      let deleteData = $(this).siblings("textarea").val('');
+
+      // remove the data from localstorage 
+      localStorage.removeItem(textId, JSON.stringify(deleteData));
+
+      $.toast({
+        text:"Data has been Delete!!",
+        icon: 'error',
+        position : 'top-right' ,
+        hideAfter : 5000, 
+        showHideTransition : 'slide'
+      })
+  })
